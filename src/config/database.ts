ac764504bbe525f0f26c755997754f71a1684fe8
João Bruno Mélo de Alencar <joaobrunoalencar@gmail.com>
2020-11-22 15:27:29 -0300

    Merged

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
 
-<<<<<<< HEAD
-=======
-console.log(process.env.MONGODB_CONNECTION);
->>>>>>> 028138428ee3843f11ddce9bc7d6c5b897444c2a
 const MONGODB_CONNECT = process.env.MONGODB_CONNECTION || '';
 
 mongoose
